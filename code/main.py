227bbcfad8b28473df7dc7f52acd43c3390840ce
ShivGrover45 <codershiv16@gmail.com>
2026-07-09 23:26:05 +0530

    add code

---
+++ code/main.py
@@ -0,0 +1,33 @@
+from settings import *
+from player import Player
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface=pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Vampire Hunters")
+        self.clock=pygame.time.Clock()
+        self.running=True
+        #group of sprites
+        self.all_sprites=pygame.sprite.Group()
+        #player
+        self.player=Player((400,300),self.all_sprites)
+
+    def run(self):
+        while self.running:
+            #dt for handling diff refresh rates
+            dt=self.clock.tick()/1000
+            #event loop
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    self.running=False
+            #update
+            self.display_surface.fill("#D8A657")
+            self.all_sprites.update(dt)
+            #drawing chars
+            self.all_sprites.draw(self.display_surface)
+            pygame.display.update()
+        pygame.quit()
+
+if __name__=="__main__":
+    game=Game()
+    game.run()
