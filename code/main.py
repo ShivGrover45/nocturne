310f8e9c089cdbb70a75df27f00f15f3b4d8f095
ShivGrover45 <codershiv16@gmail.com>
2026-07-11 14:03:45 +0530

    added world from tiled

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@ from settings import *
 from player import Player
 from tiles import *
 from random import randint
+from pytmx.util_pygame import load_pygame
 class Game:
     def __init__(self):
         pygame.init()
@@ -12,12 +13,16 @@ class Game:
         #group of sprites
         self.all_sprites=pygame.sprite.Group()
         self.collision_sprites=pygame.sprite.Group()
+        self.setup()
         #player
-        self.player=Player((400,300),self.all_sprites,self.collision_sprites)
-        #tiles
-        for x in range(6):
-            i,j=randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)
-            Tiles((i,j),(self.all_sprites,self.collision_sprites),(TILE_SIZE,TILE_SIZE))
+        self.player=Player((500,300),self.all_sprites,self.collision_sprites)
+    def setup(self):
+        map=load_pygame(join("../data","maps","world.tmx"))
+       # print(map)
+        for x,y,img in map.get_layer_by_name('Ground').tiles():
+            Ground((x*TILE_SIZE,y*TILE_SIZE),self.all_sprites,img)
+        for obj in map.get_layer_by_name('Objects'):
+            Tiles((obj.x,obj.y),(self.all_sprites,self.collision_sprites),obj.image)
 
     def run(self):
         while self.running:
--- code/tiles.py
+++ code/tiles.py
@@ -1,7 +1,12 @@
 from settings import *
 class Tiles(pygame.sprite.Sprite):
-    def __init__(self,pos,groups,size):
+    def __init__(self,pos,groups,surf):
         super().__init__(groups)
-        self.image=pygame.Surface(size)
-        self.image.fill("black")
-        self.rect=self.image.get_rect(center=pos)
+        self.image=surf
+        self.rect=self.image.get_frect(center=pos)
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self,pos,groups,surf):
+        super().__init__(groups)
+        self.image=surf
+        self.rect=self.image.get_frect(center=pos)
